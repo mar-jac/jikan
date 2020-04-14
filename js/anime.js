b54fc3a3cf36b365d06a04e9d7e7ff76d85c3b2d
mar-jac <mar.jacq0296@gmail.com>
2020-04-13 21:30:54 -0400

    Refactoring

---
+++ js/anime.js
@@ -0,0 +1,74 @@
+const baseurl = "https://api.jikan.moe/v3"
+
+function searchWeeb() {
+
+    event.preventDefault();
+
+    const form = new FormData(this);
+    const query = form.get("search");
+    
+
+    console.log(query);
+
+    fetch(`${baseurl}/search/anime?q=${query}&page=1`)
+    .then(res => res.json())
+    .then(updateDom)
+    .catch(err => {
+        console.error(err) })
+        
+}
+
+function updateDom(data)  {
+
+    const searchResults = document.getElementById('search-results');
+    
+    const animeByCategory = data.results
+    .reduce((acc, anime) => {
+        const {type} = anime;
+        if(acc[type] === undefined) acc[type] = [];
+        acc[type].push(anime);
+        return acc;
+    }, {});
+
+  searchResults.innerHTML = Object.keys(animeByCategory).map(key =>{
+
+    const animeHTML = animeByCategory[key]
+    .sort((a,b) => a.episodes - b.episodes)
+    .map(anime => {return  ` 
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="${anime.image_url}">
+        </div>
+        <div class="card-content">
+        <span class="card-title">${anime.title}</span>
+          <p>${anime.synopsis}</p>
+        </div>
+        <div class="card-action">
+          <a href="${anime.url}">More Info</a>
+         </div>
+       </div>
+      </div>
+      `
+}).join("");
+
+            return `
+            <section>
+                <h3>${key.toUpperCase()}</h3>
+            <div class="anime-row">${animeHTML}></div>
+            </section>
+            `
+    }).join("")
+
+    
+
+        
+}
+function pageLoaded()  {
+    const form = document.getElementById('search_form');
+    form.addEventListener("submit", searchWeeb)
+
+}
+
+
+window.addEventListener("load", pageLoaded)
